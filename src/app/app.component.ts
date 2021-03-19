2215e3d62799d7efd86ed637bdda572a7ec95568
Brayan Giraldo <bgiraldo020@gmail.com>
2021-03-19 09:05:01 -0500

    Se termina el proyecto de Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+    nombre = `Brayan`;
+    nombre2 = `Kai'sa, La cazadora del vacio`;
+
+    arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    PI = Math.PI;
+
+    porcentaje = 0.235;
+
+    salario = 1234.5;
+
+    campeon = {
+        nombre: `Kai'sa`,
+        rol: 'Tirador',
+        edad: 25,
+        ubicacion: {
+            lugar: 'El vacio',
+            pais: 'Shurima'
+        }
+    };
+
+    valorPromesa = new Promise<string>((resolve, reject ) => {
+        setTimeout(() => {
+            resolve('A dormir!!');
+        }, 1500);
+    });
+
+    fecha = new Date();
+
+    idioma = 'ja';
+
+    videoUrl = 'https://www.youtube.com/embed/RLWcYADoV84';
+
+        activar = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import localeJA from '@angular/common/locales/ja';
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomSeguroPipe } from './pipes/dom-seguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+registerLocaleData(localeJA);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CapitalizadoPipe,
+        DomSeguroPipe,
+        ContrasenaPipe
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        {
+            provide: LOCALE_ID,
+            useValue: 'es'
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
